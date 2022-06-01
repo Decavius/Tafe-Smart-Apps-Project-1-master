038d472130fa899ceeed9436b313374b5c46b99b
Decavium <leokabirwilliams@gmail.com>
2022-06-01 13:33:20 +1000

    Updated label syntax

--- ui/components/Label.js
+++ ui/components/Label.js
@@ -2,5 +2,6 @@ import { Text } from "react-native";
 import theme from "../style";
 
 export default function Label(props){
-    return (<Text style={theme.label}>{props.value}</Text>)
+    return (<Text style={theme.label}>{props.value}
+    </Text>)
 }
